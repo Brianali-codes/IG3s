b8bb1e5501a438aeeda81c4eee2934f4305f4b64
gamerfunky78 <gamerfunky78@gmail.com>
2024-09-12 18:30:15 +0300

    added instagram downloader

--- public/index.js
+++ public/index.js
@@ -4,9 +4,12 @@ async function getVideo() {
 
     let input = document.getElementById('display').value
     let loader = document.getElementById("output")
-
+    let Button = document.getElementById("download2")
+    let Button2 = document.getElementById("download2")
 
     loader.style.display = "flex"
+    Button.style.display = "flex"
+    Button2.style.display = "none"
 
     const url = `https://yt-search-and-download-mp3.p.rapidapi.com/mp3?url=${input}`
     const options = {
@@ -80,6 +83,65 @@ async function getVideo() {
 }
 }
 
+async function getIgVideo(){
+    let input = document.getElementById("display2").value
+    let Button = document.getElementById("download")
+    let Button2 = document.getElementById("download2")
+    let OP = document.getElementById("output")
+
+
+    OP.style.display = "flex"
+    Button.style.display = "none"
+    Button2.style.display = "flex"
+    
 
 
 
+    const url = `https://instagram-downloader32.p.rapidapi.com/instagram-new?instaUrl=${input}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '2cfe58baa1mshff2541554fc663cp12592cjsne4abb2a42866',
+            'x-rapidapi-host': 'instagram-downloader32.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+
+        let thumbnail = document.getElementById("VDIMG")
+        thumbnail.style.display = "flex"
+        thumbnail.src = result[0].cover
+        thumbnail.style.width = "300px"
+        let link = document.createElement("a")
+        link.appendChild(Button2)
+        link.href = result[0].download_link
+        thumbnail.style.borderRadius = "10px"
+        OP.appendChild(link)
+
+
+    } catch (error) {
+        console.error(error);
+}
+}
+
+function toggleDownloader(){
+    let YTM3 = document.getElementById("YTM")
+    let IGTM = document.getElementById("IGdownloader")
+    let img = document.getElementById("VDIMG")
+
+    if (IGTM.style.display === "none" || IGTM.style.display === ""){
+        YTM3.style.display ="none"
+        IGTM.style.display = "flex"
+        img.innerHTML = ""
+    }
+    else{
+        IGTM.style.display = "none"
+        YTM3.style.display ="flex"
+        img.innerHTML = ""
+    }
+
+
+}
